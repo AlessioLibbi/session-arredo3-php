3efecdf89294b010a1ce88a9bd34135164070005
Alessio Libbi <alex.libbi@gmail.com>
2023-10-26 17:22:28 +0200

    Add: work in progress on crude with object and section

--- index.php
+++ index.php
@@ -1,3 +1,86 @@
+<?php 
+
+
+class Machine {
+    private $id;
+    private $name;
+
+
+
+    function __construct( $name)
+    {
+        $this->id = uniqid();
+        $this->name = $name;
+       
+    }
+    function getId() {
+        return $this->id;
+    }
+    function getName() {
+        return $this->name;
+    }
+}
+
+interface IStoreMachine {
+    function add($name);
+    function get();
+    function edit($id, $name);
+    function delete($id);
+};
+
+class MachineInSession implements IStoreMachine {
+    
+
+    function __construct()
+    {
+        session_start();
+
+        if(!isset($_SESSION["machines"])) {
+            $_SESSION["machines"] = array();
+            $mach = $_SESSION["machine"];
+            return $mach;
+        }
+
+    }
+
+    function add($name){
+
+        array_push($mach, new Machine($name));
+        
+    }
+    function get(){
+        $mach = $_SESSION["machine"];
+        return $mach;
+    }
+    function edit($id, $name){
+
+    }
+    function delete($id){
+
+    }
+
+}
+$machineRepo = new MachineInSession();
+
+
+
+
+
+if(isset($_POST['action'])){
+    if($_POST['action'] == 'create') {
+        $machineRepo->add($_POST['name']);
+    } else if ($_POST['action'] == 'update') {
+        $machineRepo->edit($_POST['id'], $_POST['name']);
+    } else if ($_POST['action'] == 'delete') {
+        $machineRepo->delete($_POST['id']);
+    }
+}
+
+$machines = $machineRepo->get();
+// var_dump($machines);
+
+
+?>
 
 
 <!DOCTYPE html>
@@ -9,14 +92,28 @@
 </head>
 <body>
     <h1>Questa e la tua rosa vuoi aggiungere qualcuno ?</h1>
-    <form action="script.php" method="POST">
-        
-        <label for="name">
-            <input name="name" id="name" type="text">
-        </label>
-       
-        <button type="submit">Aggiungilo alla rosa</button>
+    <div>
+        <?php foreach ($machines as $machine) {  ?>
+        <form action="index.php" method="POST">
+            <input type="hidden" name="action" value="update">
+            <input type="hidden" name="id" value="<?php echo $machine->getId();?>">
+            <input type="text" name="name" value="<?php echo $machine->getName();?>">
+            <input type="submit" value="UPDATE">
+        </form>
+        <form action="index.php" method="POST">
+            <input type="hidden" name="action" value="delete">
+            <input type="hidden" name="id" value="<?php echo $machine->getId();?>">
+            <input type="submit" value="DELETE">
+            
+        </form>
+        <?php } ?>
+    </div>
+    <form action="index.php" method="POST">
+        <input type="hidden" name="action" value="create">
+        <input type="text" name="name">
+        <input type="submit" value="CREATE">
     </form>
+  
     
 
    
